2d381aa87169f5a5475b2a9d0dadddd8db30e58c
Paawan Katiyar <paawankatiyar612006@gmail.com>
2025-09-05 19:38:03 +0530

    final commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <img src="/images/tumblr_1444a2c7969ed5f9107ab48a89a7035a_07f02187_400-modified.jpg" alt=""/>
         <img src="/images/tumblr_216c2b9a58a62c520e4d587762011982_43f386cd_400-modified.jpg" alt=""/>
         <img src="/images/tumblr_4c1882938d4ca29e9227228519c4c6d9_3bba170b_400-modified.jpg" alt=""/>
-        <img src="/images/col/tumblr_f2b6121aeb0bbcf9a05d9a70b368b59a_ac87d5fet1_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_f2b6121aeb0bbcf9a05d9a70b368b59a_ac87d5fet1_400-modified.jpg" alt=""/>
         <p className = "absolute bottom-[-25px] text-[#D9D9D4] text-5xl fancy z-10">A quiet corner for readers and dreamers alike.</p>
         <div className = "absolute bg-[#1C1C1B] w-400 h-200 z-9 opacity-80">.</div>
       </div>
