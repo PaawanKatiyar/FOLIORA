720e57865275df9c5d1b641b7da91b7fea80888f
Paawan Katiyar <paawankatiyar612006@gmail.com>
2025-09-05 19:20:26 +0530

    initial commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,18 +2,18 @@ const Hero = () => {
   return (
     <div className = "min-h-[500px] pb-20 pt-7 bg-[#D9D9D4]">
       <div className = "flex justify-center relative">
-        <img src="../public/images/tumblr_1444a2c7969ed5f9107ab48a89a7035a_07f02187_400-modified.jpg" alt=""/>
-        <img src="../public/images/tumblr_216c2b9a58a62c520e4d587762011982_43f386cd_400-modified.jpg" alt=""/>
-        <img src="../public/images/tumblr_4c1882938d4ca29e9227228519c4c6d9_3bba170b_400-modified.jpg" alt=""/>
-        <img src="../public/images/col/tumblr_f2b6121aeb0bbcf9a05d9a70b368b59a_ac87d5fet1_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_1444a2c7969ed5f9107ab48a89a7035a_07f02187_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_216c2b9a58a62c520e4d587762011982_43f386cd_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_4c1882938d4ca29e9227228519c4c6d9_3bba170b_400-modified.jpg" alt=""/>
+        <img src="/images/col/tumblr_f2b6121aeb0bbcf9a05d9a70b368b59a_ac87d5fet1_400-modified.jpg" alt=""/>
         <p className = "absolute bottom-[-25px] text-[#D9D9D4] text-5xl fancy z-10">A quiet corner for readers and dreamers alike.</p>
         <div className = "absolute bg-[#1C1C1B] w-400 h-200 z-9 opacity-80">.</div>
       </div>
       <div className = "flex justify-center">
-        <img src="../public/images/col/tumblr_1104e25d4b8d1cfe5d3580b426c5656d_433f00c0b1_400-modified.jpg" alt=""/>
-        <img src="../public/images/col/tumblr_61215b29d5c089895e6f6e76a1bab63f_057acfef_400-modified.jpg" alt=""/>
-        <img src="../public/images/col/tumblr_6740ca375f0c573706a5a6bbeeb8b6ee_d1bc4de3_400-modified.jpg" alt=""/>
-        <img src="../public/images/col/tumblr_9bf4163a024d352a24d293026e87901a_5d9ad64f_400-modified.jpg" alt=""/>
+        <img src="/images/col/tumblr_1104e25d4b8d1cfe5d3580b426c5656d_433f00c0b1_400-modified.jpg" alt=""/>
+        <img src="/images/col/tumblr_61215b29d5c089895e6f6e76a1bab63f_057acfef_400-modified.jpg" alt=""/>
+        <img src="/images/col/tumblr_6740ca375f0c573706a5a6bbeeb8b6ee_d1bc4de3_400-modified.jpg" alt=""/>
+        <img src="/images/col/tumblr_9bf4163a024d352a24d293026e87901a_5d9ad64f_400-modified.jpg" alt=""/>
       </div>
     </div>
   )
