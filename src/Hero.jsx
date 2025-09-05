4acc45bb2e33d98f9ff9bb94d2bb53e1224ff7c7
Paawan Katiyar <paawankatiyar612006@gmail.com>
2025-09-05 19:31:36 +0530

    vercel commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
         <div className = "absolute bg-[#1C1C1B] w-400 h-200 z-9 opacity-80">.</div>
       </div>
       <div className = "flex justify-center">
-        <img src="/images/col/tumblr_1104e25d4b8d1cfe5d3580b426c5656d_433f00c0b1_400-modified.jpg" alt=""/>
-        <img src="/images/col/tumblr_61215b29d5c089895e6f6e76a1bab63f_057acfef_400-modified.jpg" alt=""/>
-        <img src="/images/col/tumblr_6740ca375f0c573706a5a6bbeeb8b6ee_d1bc4de3_400-modified.jpg" alt=""/>
-        <img src="/images/col/tumblr_9bf4163a024d352a24d293026e87901a_5d9ad64f_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_1104e25d4b8d1cfe5d3580b426c5656d_433f00c0b1_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_61215b29d5c089895e6f6e76a1bab63f_057acfef_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_6740ca375f0c573706a5a6bbeeb8b6ee_d1bc4de3_400-modified.jpg" alt=""/>
+        <img src="/images/tumblr_9bf4163a024d352a24d293026e87901a_5d9ad64f_400-modified.jpg" alt=""/>
       </div>
     </div>
   )
--- src/Post.jsx
+++ src/Post.jsx
@@ -7,7 +7,7 @@ const Post = () => {
       </div>
       <div className = "flex justify-center gap-25 mt-12">
         <div>
-          <img className = "w-100 h-100" src="../public/images/post/@g_readss on insta 🌷.jpg" alt=""/>
+          <img className = "w-100 h-100" src="/images/@g_readss on insta 🌷.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">on 24.Aug.2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">BOOKS THAT FEEL LIKE A WARM HUG</p>
@@ -20,7 +20,7 @@ const Post = () => {
           </div>
         </div>
         <div>
-          <img className = "w-100 h-100"  src="../public/images/post/Atomic Habits.jpg" alt=""/>
+          <img className = "w-100 h-100"  src="/images/Atomic Habits.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">On 23.Aug.2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">TURN THE PAGE, CHANGE YOUR LIFE</p>
@@ -33,7 +33,7 @@ const Post = () => {
           </div>
         </div>
         <div>
-          <img className = "w-100 h-100"  src="../public/images/post/clarie biggs - taming 7.jpg" alt=""/>
+          <img className = "w-100 h-100"  src="/images/clarie biggs - taming 7.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">On 22.Aug.2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">YOUNG ADULT BOOKS YOU WON'T PUT DOWN</p>
@@ -48,7 +48,7 @@ const Post = () => {
       </div>
       <div className = "flex justify-center gap-25 mt-12">
         <div>
-          <img className = "w-100 h-100" src="../public/images/post/Register - Login.jpg" alt=""/>
+          <img className = "w-100 h-100" src="/images/Register - Login.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">On 21. Aug. 2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">TIMELESS STORIES THAT NEVER GROW OLD</p>
@@ -61,7 +61,7 @@ const Post = () => {
           </div>
         </div>
         <div>
-          <img className = "w-100 h-100" src="../public/images/post/asphyvx on instagram.jpg" alt=""/>
+          <img className = "w-100 h-100" src="/images/asphyvx on instagram.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">On 20.Aug.2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">MOODY AND MYSTERIOUS: BEST DARK ACADEMIA BOOKS</p>
@@ -74,7 +74,7 @@ const Post = () => {
           </div>
         </div>
         <div>
-          <img className = "w-100 h-100" src="../public/images/post/classic.jpg" alt=""/>
+          <img className = "w-100 h-100" src="/images/classic.jpg" alt=""/>
           <div className = "text-left">
             <p className = "text-sm fancy mt-4 text-[#8C6B4E]">On 19.Aug.2025</p>
             <p className = "text-3xl w-100 break-normal head-font mb-4 mt-1 hover:text-[#8C6B4E] transition-colors duration-300 ease-in-out">BOOKS THAT KEEP YOU GUESSING TILL THE END</p>
